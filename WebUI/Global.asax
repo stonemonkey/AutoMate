39b4ccad699d0e36eedf5e4afc443474a688f91c
stonemonkey <stonemonkey77ro@yahoo.com>
2013-11-16 11:23:55 +0200

    rename project WebUI to Ui.Web.

--- WebUI/Global.asax
+++ WebUI/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebUI.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ui.Web.MvcApplication" Language="C#" %>
--- WebUI/Global.asax.cs
+++ WebUI/Global.asax.cs
@@ -2,8 +2,9 @@
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Ui.Web.App_Start;
 
-namespace WebUI
+namespace Ui.Web
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
