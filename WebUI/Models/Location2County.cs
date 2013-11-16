499beddaecf5148149762a06c009b89164bd1176
Adrian Barglazan <jm_aba@yahoo.com>
2013-11-16 18:11:25 +0200

    Small refactoring

---
+++ WebUI/Models/Location2County.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Xml;
+
+namespace WebUI.Models
+{
+    public class Location2County
+    {
+        public static string GetLocationAdminDistrictName(string locationName)
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    var result = client.DownloadString(GetUrl(locationName));
+
+                    var xmlDocument = new XmlDocument();
+                    xmlDocument.LoadXml(result);
+                    var districtName = xmlDocument.GetElementsByTagName("AdminDistrict");
+                    if (districtName.Count != 0)
+                    {
+                        if (!string.IsNullOrEmpty(districtName[0].InnerText))
+                            return districtName[0].InnerText;
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+            return string.Empty;
+        }
+
+        private static string GetUrl(string locationName)
+        {
+            return
+                string.Format(
+                    Configuration.UrlLocationBase,
+                    locationName,
+                    Configuration.BingMapsAuthenticationKey);
+        }
+    }
+}
--- WebUI/Scripts/main.js
+++ WebUI/Scripts/main.js
@@ -1,73 +1,86 @@
-﻿
-var map = null;
-var myToolbox = null;
+﻿var map = null;
+var clientStatisticsItems = [];
 
-fromXMLString = function (strXml) {
-    if (window.DOMParser) {
-        return jQuery(new DOMParser().parseFromString(strXml, "text/xml"));
-    } else if (window.ActiveXObject) {
-        var doc = new ActiveXObject("Microsoft.XMLDOM");
-        doc.async = "false";
-        doc.loadXML(strXml);
-        return jQuery(doc);
-    } else {
-        return jQuery(strXml);
+function getAgresivityRateByLocation(locationName) {
+    for (var i = 0, len = clientStatisticsItems.length; i < len; i++) {
+
+        if (clientStatisticsItems[i].Location === locationName)
+            return clientStatisticsItems[i].AgresivityRate;
     }
-};
+    return 0;
+}
 
 function getMap() {
-    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-        credentials: 'Aq1q9FCxgHiuqrRe3NywYeSb1BPb-q6eyuyYNHe2rg6-XLds9hzYrAiDKwNBQvZY',
-        enableClickableLogo: false,
-    });
-
-    map.setView({ zoom: 6, center: new Microsoft.Maps.Location(45.867063, 24.916992) });
-
     $.ajax({
-        url: 'Home/GetRoKmlData',
-        traditional: true,
+        type: "GET",
+        url: "Home/BingMapsAuthenticationKey",
         success: function (data) {
-            var xml = $(data);
-            xml.find('coordinates').each(function () {
-                var res = this.textContent.split('\n');
-                var countyName = $(this).parent().parent().parent().parent().parent().find('name').text();
-                var arrayCollection = [];
-                $(res).each(function () {
-                    var loc = this.split(',');
+            map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+                credentials: data   ,
+                enableClickableLogo: false,
+            });
 
-                    if (parseFloat(loc[0]) && parseFloat(loc[1])) {
-                        var location = new Microsoft.Maps.Location(parseFloat(loc[1]), parseFloat(loc[0]));
-                        arrayCollection.push(location);
+            Microsoft.Maps.loadModule('Microsoft.Maps.Search', {});
+
+            map.setView({ zoom: 6, center: new Microsoft.Maps.Location(45.867063, 24.916992) });
+
+            $.getJSON('Home/GetLocationAgresivtyRates', function (data) {
+
+                $.each(data, function (index, item) {
+                    clientStatisticsItems.push(item);
+                });
+
+                $.ajax({
+                    url: 'Home/GetRoKmlData',
+                    traditional: true,
+                    success: function (data) {
+                        var xml = $(data);
+                        xml.find('coordinates').each(function () {
+                            var res = this.textContent.split('\n');
+                            var countyName = $(this).parent().parent().parent().parent().parent().find('name').text();
+                            var arrayCollection = [];
+                            $(res).each(function () {
+                                var loc = this.split(',');
+
+                                if (parseFloat(loc[0]) && parseFloat(loc[1])) {
+                                    var location = new Microsoft.Maps.Location(parseFloat(loc[1]), parseFloat(loc[0]));
+                                    arrayCollection.push(location);
+                                }
+                            });
+
+                            var color = new Microsoft.Maps.Color(getAgresivityRateByLocation(countyName), 255, 0, 0);
+
+                            var line = new Microsoft.Maps.Polygon(arrayCollection, {
+                                fillColor: color,
+                                strokeColor: new Microsoft.Maps.Color(200, 0, 100, 100),
+                                strokeThickness: 1
+                            });
+                            line.countyName = countyName;
+
+                            Microsoft.Maps.Events.addHandler(line, 'mouseover', function () {
+                                line.setOptions({
+                                    fillColor: new Microsoft.Maps.Color(100, 100, 0, 100),
+                                    strokeColor: new Microsoft.Maps.Color(200, 0, 100, 100),
+                                    strokeThickness: 3
+                                });
+                            });
+
+                            Microsoft.Maps.Events.addHandler(line, 'mouseout', function () {
+                                line.setOptions({
+                                    fillColor: color,
+                                    strokeColor: new Microsoft.Maps.Color(200, 0, 100, 100),
+                                    strokeThickness: 1
+                                });
+                            });
+
+                            Microsoft.Maps.Events.addHandler(line, 'click', function () {
+                                $('#county').html(line.countyName);
+                            });
+
+                            map.entities.push(line);
+                        });
                     }
                 });
-                var line = new Microsoft.Maps.Polygon(arrayCollection, {
-                    fillColor: new Microsoft.Maps.Color(100, 100, 0, 100),
-                    strokeColor: new Microsoft.Maps.Color(200, 0, 100, 100),
-                    strokeThickness: 1
-                });
-                line.countyName = countyName;
-
-                Microsoft.Maps.Events.addHandler(line, 'mouseover', function () {
-                    line.setOptions({
-                        fillColor: new Microsoft.Maps.Color(100, 100, 0, 100),
-                        strokeColor: new Microsoft.Maps.Color(200, 0, 100, 100),
-                        strokeThickness: 3
-                    });
-                });
-
-                Microsoft.Maps.Events.addHandler(line, 'mouseout', function () {
-                    line.setOptions({
-                        fillColor: new Microsoft.Maps.Color(100, 100, 0, 100),
-                        strokeColor: new Microsoft.Maps.Color(200, 0, 100, 100),
-                        strokeThickness: 1
-                    });
-                });
-
-                Microsoft.Maps.Events.addHandler(line, 'click', function () {
-                    $('#county').html(line.countyName);
-                });
-
-                map.entities.push(line);
             });
         }
     });
@@ -76,3 +89,5 @@ function getMap() {
 $(function () {
     getMap();
 });
+
+
