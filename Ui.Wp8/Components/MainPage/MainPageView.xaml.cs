55dd76c912b31fee60fd7c2081e9cb637aa3f8c2
stonemonkey <stonemonkey77ro@yahoo.com>
2013-11-16 14:21:55 +0200

    added button on first page for starting the measurements.

--- Ui.Wp8/Components/MainPage/MainPageView.xaml.cs
+++ Ui.Wp8/Components/MainPage/MainPageView.xaml.cs
@@ -1,101 +1,13 @@
-﻿using System;
-using System.Windows;
-using Microsoft.Phone.Controls;
-using Microsoft.Devices.Sensors;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Phone.Controls;
 
 namespace Ui.Wp8.Components.MainPage
 {
     public partial class MainPageView : PhoneApplicationPage
     {
-        Accelerometer accelerometer;
-
         // Constructor
         public MainPageView()
         {
             InitializeComponent();
-
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
-            if (!Accelerometer.IsSupported)
-            {
-                // The device on which the application is running does not support
-                // the accelerometer sensor. Alert the user and disable the
-                // Start and Stop buttons.
-                statusTextBlock.Text = "device does not support accelerometer";
-                startButton.IsEnabled = false;
-                stopButton.IsEnabled = false;
-            }
-        }
-
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
-
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
-        private void startButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (accelerometer == null)
-            {
-                // Instantiate the Accelerometer.
-                accelerometer = new Accelerometer();
-                accelerometer.TimeBetweenUpdates = TimeSpan.FromMilliseconds(20);
-                accelerometer.CurrentValueChanged +=
-                    new EventHandler<SensorReadingEventArgs<AccelerometerReading>>(accelerometer_CurrentValueChanged);
-            }
-            try
-            {
-                statusTextBlock.Text = "starting accelerometer.";
-                accelerometer.Start();
-            }
-            catch (InvalidOperationException ex)
-            {
-                statusTextBlock.Text = "unable to start accelerometer.";
-            }
-        }
-        
-        void accelerometer_CurrentValueChanged(object sender, SensorReadingEventArgs<AccelerometerReading> e)
-        {
-            // Call UpdateUI on the UI thread and pass the AccelerometerReading.
-            Dispatcher.BeginInvoke(() => UpdateUI(e.SensorReading));
-        }
-
-        private void stopButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (accelerometer != null)
-            {
-                // Stop the accelerometer.
-                accelerometer.Stop();
-                statusTextBlock.Text = "accelerometer stopped.";
-            }
-        }
-
-        private void UpdateUI(AccelerometerReading accelerometerReading)
-        {
-            statusTextBlock.Text = "getting data";
-
-            Vector3 acceleration = accelerometerReading.Acceleration;
-
-            // Show the numeric values.
-            xTextBlock.Text = "X: " + acceleration.X.ToString("0.00");
-            yTextBlock.Text = "Y: " + acceleration.Y.ToString("0.00");
-            zTextBlock.Text = "Z: " + acceleration.Z.ToString("0.00");
-
-            // Show the values graphically.
-            xLine.X2 = xLine.X1 + acceleration.X * 200;
-            yLine.Y2 = yLine.Y1 - acceleration.Y * 200;
-            zLine.X2 = zLine.X1 - acceleration.Z * 100;
-            zLine.Y2 = zLine.Y1 + acceleration.Z * 100;
         }
     }
 }
