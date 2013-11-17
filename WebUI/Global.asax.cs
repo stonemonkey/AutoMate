bed305050c77200af116c25bec8d23b145641b88
Adrian Barglazan <jm_aba@yahoo.com>
2013-11-17 02:07:39 +0200

    aaaa

--- WebUI/Global.asax.cs
+++ WebUI/Global.asax.cs
@@ -6,6 +6,7 @@ using Ui.Web.App_Start;
 
 namespace Ui.Web
 {
+    //todo
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
