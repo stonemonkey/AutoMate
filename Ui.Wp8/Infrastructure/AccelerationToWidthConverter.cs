f46d71c484940372580147612bfb6cd23d24a42b
stonemonkey <stonemonkey77ro@yahoo.com>
2013-11-17 08:40:59 +0200

    ui ...

--- Ui.Wp8/Infrastructure/AccelerationToWidthConverter.cs
+++ Ui.Wp8/Infrastructure/AccelerationToWidthConverter.cs
@@ -1,29 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
-using Ui.Wp8.Infrastructure;
 
-namespace Ui.Wp8.Components.RecordingPage
+namespace Ui.Wp8.Infrastructure
 {
     public class AccelerationToWidthConverter: IValueConverter
     {
-        private const int maximumWidth = 400;
-        private const double maximumAcceleration = 2;
+        private const int MaximumWidth = 400;
+        private const double MaximumAcceleration = 2;
 
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             var acceleration = (double)value;
-            int width = (int)((acceleration * maximumWidth) / maximumAcceleration);
+            int width = (int)((acceleration * MaximumWidth) / MaximumAcceleration);
             return Math.Abs(width);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             var width = (double)value;
-            double acceleration = (width * maximumAcceleration) / maximumWidth;
+            double acceleration = (width * MaximumAcceleration) / MaximumWidth;
             return acceleration;
         }
     }
--- Ui.Wp8/Infrastructure/SpeedToAngleConverter.cs
+++ Ui.Wp8/Infrastructure/SpeedToAngleConverter.cs
@@ -1,31 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
 
-namespace Ui.Wp8.Components.RecordingPage
+namespace Ui.Wp8.Infrastructure
 {
     public class SpeedToAngleConverter: IValueConverter
     {
-        private const double startAngle = 45;
-        private const double stopAngle = 315;
-        private const double topSpeed = 72.22;
+        private const double StartAngle = 45;
+        private const double StopAngle = 315;
+        private const double TopSpeed = 72.22;
 
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             var speed = (double)value;            
             if (double.IsNaN(speed)) return 0;
             
-            var result = speed * stopAngle / topSpeed + startAngle;
+            var result = speed * StopAngle / TopSpeed + StartAngle;
             return (int) result;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            var angle = (double)value - startAngle;
-            return angle * topSpeed / stopAngle;
+            var angle = (double)value - StartAngle;
+            return angle * TopSpeed / StopAngle;
         }
     }
 }
